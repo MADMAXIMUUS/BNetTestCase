7ab94e0432d3029820c95a871d066d7465559e91
MADMAXIMUUS <maxim00nechaev@gmail.com>
2023-04-16 00:42:08 +0300

    Add app module di

---
+++ app/src/main/java/ru/madmax/bnettestcase/di/AppModule.kt
@@ -0,0 +1,43 @@
+package ru.madmax.bnettestcase.di
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.madmax.bnettestcase.data.dataSource.DragApi
+import ru.madmax.bnettestcase.util.Constants.BASE_URL
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDragApi(client: OkHttpClient): DragApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+            .create(DragApi::class.java)
+    }
+
+}
